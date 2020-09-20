2c36d6188aa636b19d0b5f04926678c749bef5df
mova <valeriy.molchanov.77@gmail.com>
2020-09-20 13:48:52 +0300

    remove require ./lib/data

--- index.js
+++ index.js
@@ -9,39 +9,7 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
-const _data = require('./lib/data');
 const handlers = require('./lib/handlers');
-
-// _data.create('test', 'newfile', { foo: 'bar' }, function (err) {
-//   console.log({ err });
-// });
-// _data.create('test', 'newfile', { foo: 'bar' }, function (err) {
-//   console.log({ err });
-// });
-// _data.read('test', 'newfile', function (err, data) {
-//   console.log({ err, data });
-// });
-// _data.read('test', 'newfile', function (err, data) {
-//   console.log({ err, data });
-// });
-// _data.read('test', 'newfileX', function (err, data) {
-//   console.log({ err, data });
-// });
-// _data.read('test', 'newfileX', function (err, data) {
-//   console.log({ err, data });
-// });
-// _data.update('test', 'newfile', { fizz: 'buzz' }, function (err) {
-//   console.log({ err });
-// });
-// _data.update('test', 'newfile', { fizz: 'buzz' }, function (err) {
-//   console.log({ err });
-// });
-// _data.delete('test', 'newfile', function (err) {
-//   console.log({ err });
-// });
-// _data.delete('test', 'newfile', function (err) {
-//   console.log({ err });
-// });
 
 const unifedServer = function (req, res) {
   let parsedUrl = url.parse(req.url, true);
